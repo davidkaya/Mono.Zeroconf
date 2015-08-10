e775a41c546d50e63f1b32bbe29796bb795529c3
Kaya David <David.Kaya@ysoft.com>
2015-08-10 12:38:38 +0200

    Fixed byte endianity

--- src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/BrowseService.cs
+++ src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/BrowseService.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -38,22 +38,22 @@ namespace Mono.Zeroconf.Providers.Bonjour
     {
         private bool is_resolved = false;
         private bool resolve_pending = false;
-        
+
         private Native.DNSServiceResolveReply resolve_reply_handler;
         private Native.DNSServiceQueryRecordReply query_record_reply_handler;
-        
+
         public event ServiceResolvedEventHandler Resolved;
 
         public BrowseService()
         {
             SetupCallbacks();
         }
-        
+
         public BrowseService(string name, string replyDomain, string regtype) : base(name, replyDomain, regtype)
         {
             SetupCallbacks();
         }
-        
+
         private void SetupCallbacks()
         {
             resolve_reply_handler = new Native.DNSServiceResolveReply(OnResolveReply);
@@ -64,95 +64,95 @@ namespace Mono.Zeroconf.Providers.Bonjour
         {
             Resolve(false);
         }
-        
+
         public void Resolve(bool requery)
         {
             if(resolve_pending) {
                 return;
             }
-        
+
             is_resolved = false;
             resolve_pending = true;
-            
+
             if(requery) {
                 InterfaceIndex = 0;
             }
-        
+
             ServiceRef sd_ref;
-            ServiceError error = Native.DNSServiceResolve(out sd_ref, ServiceFlags.None, 
+            ServiceError error = Native.DNSServiceResolve(out sd_ref, ServiceFlags.None,
                 InterfaceIndex, Name, RegType, ReplyDomain, resolve_reply_handler, IntPtr.Zero);
-                
+
             if(error != ServiceError.NoError) {
                 throw new ServiceErrorException(error);
             }
-            
+
             sd_ref.Process();
         }
-        
+
         public void RefreshTxtRecord()
         {
             // Should probably make this async?
-        
+
             ServiceRef sd_ref;
             ServiceError error = Native.DNSServiceQueryRecord(out sd_ref, ServiceFlags.None, 0,
                 fullname, ServiceType.TXT, ServiceClass.IN, query_record_reply_handler, IntPtr.Zero);
-                
+
             if(error != ServiceError.NoError) {
                 throw new ServiceErrorException(error);
             }
-            
+
             sd_ref.Process();
         }
-        
+
         private void OnResolveReply(ServiceRef sdRef, ServiceFlags flags, uint interfaceIndex,
-            ServiceError errorCode, string fullname, string hosttarget, ushort port, ushort txtLen, 
+            ServiceError errorCode, string fullname, string hosttarget, ushort port, ushort txtLen,
             IntPtr txtRecord, IntPtr contex)
         {
             is_resolved = true;
             resolve_pending = false;
-            
+
             InterfaceIndex = interfaceIndex;
             FullName = fullname;
-            this.port = port;
+            this.port = (ushort)IPAddress.NetworkToHostOrder((short)port);
             TxtRecord = new TxtRecord(txtLen, txtRecord);
 
             sdRef.Deallocate();
-            
+
             // Run an A query to resolve the IP address
             ServiceRef sd_ref;
-            
+
             if (AddressProtocol == AddressProtocol.Any || AddressProtocol == AddressProtocol.IPv4) {
                 ServiceError error = Native.DNSServiceQueryRecord(out sd_ref, ServiceFlags.None, interfaceIndex,
                     hosttarget, ServiceType.A, ServiceClass.IN, query_record_reply_handler, IntPtr.Zero);
-                
+
                 if(error != ServiceError.NoError) {
                     throw new ServiceErrorException(error);
                 }
-            
+
                 sd_ref.Process();
             }
-            
+
             if (AddressProtocol == AddressProtocol.Any || AddressProtocol == AddressProtocol.IPv6) {
                 ServiceError error = Native.DNSServiceQueryRecord(out sd_ref, ServiceFlags.None, interfaceIndex,
                     hosttarget, ServiceType.AAAA, ServiceClass.IN, query_record_reply_handler, IntPtr.Zero);
-                
+
                 if(error != ServiceError.NoError) {
                     throw new ServiceErrorException(error);
                 }
-            
+
                 sd_ref.Process();
             }
         }
-     
+
         private void OnQueryRecordReply(ServiceRef sdRef, ServiceFlags flags, uint interfaceIndex,
-            ServiceError errorCode, string fullname, ServiceType rrtype, ServiceClass rrclass, ushort rdlen, 
+            ServiceError errorCode, string fullname, ServiceType rrtype, ServiceClass rrclass, ushort rdlen,
             IntPtr rdata, uint ttl, IntPtr context)
         {
             switch(rrtype) {
                 case ServiceType.A:
                     IPAddress address;
 
-                    if(rdlen == 4) {   
+                    if(rdlen == 4) {
                         // ~4.5 times faster than Marshal.Copy into byte[4]
                         uint address_raw = (uint)(Marshal.ReadByte (rdata, 3) << 24);
                         address_raw |= (uint)(Marshal.ReadByte (rdata, 2) << 16);
@@ -172,7 +172,7 @@ namespace Mono.Zeroconf.Providers.Bonjour
                         hostentry = new IPHostEntry();
                         hostentry.HostName = hosttarget;
                     }
-                    
+
                     if(hostentry.AddressList != null) {
                         ArrayList list = new ArrayList(hostentry.AddressList);
                         list.Add(address);
@@ -180,30 +180,29 @@ namespace Mono.Zeroconf.Providers.Bonjour
                     } else {
                         hostentry.AddressList = new IPAddress [] { address };
                     }
-                    
+
                     ServiceResolvedEventHandler handler = Resolved;
                     if(handler != null) {
                         handler(this, new ServiceResolvedEventArgs(this));
                     }
-                    
+
                     break;
                 case ServiceType.TXT:
                     if(TxtRecord != null) {
                         TxtRecord.Dispose();
                     }
-            
+
                     TxtRecord = new TxtRecord(rdlen, rdata);
                     break;
                 default:
                     break;
             }
-            
+
             sdRef.Deallocate();
         }
-        
+
         public bool IsResolved {
             get { return is_resolved; }
         }
     }
 }
-
